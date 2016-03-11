22147365a4c7aa91f6913550fabc590f6d3172e2
nebtrx <neb.trx@gmail.com>
2016-03-11 16:25:53 -0500

    Remove unnecessary return statement

--- lib/twiml_generator.rb
+++ lib/twiml_generator.rb
@@ -1,6 +1,6 @@
 module TwimlGenerator
   def self.generate_connect_conference(call_sid, wait_url, start_on_enter, end_on_exit)
-    return Twilio::TwiML::Response.new do |r|
+    Twilio::TwiML::Response.new do |r|
       r.Dial do |d|
         d.Conference call_sid, startConferenceOnEnter: start_on_enter,
                                endConferenceOnExit: end_on_exit,
@@ -10,7 +10,7 @@ module TwimlGenerator
   end
 
   def self.generate_wait
-    return Twilio::TwiML::Response.new do |r|
+    Twilio::TwiML::Response.new do |r|
       r.Say 'Thank you for calling. Please wait in line for a few seconds. An agent will be with you shortly.'
       r.Play 'http://com.twilio.music.classical.s3.amazonaws.com/BusyStrings.mp3'
     end.to_xml
