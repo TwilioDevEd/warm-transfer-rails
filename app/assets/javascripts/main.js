c159c91210a7971c1c0d564bc38594a4ce378c70
nebtrx <neb.trx@gmail.com>
2016-03-14 10:23:23 -0500

    Minor refactor in main.js file

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -9,25 +9,10 @@ $(function() {
   var $hangupCallButton = $("#hangup-call-button");
   var $dialAgent2Button = $("#dial-agent2-button");
 
-  $connectAgent1Button.on('click', { agentId: 'agent1' }, agentClick);
-  $connectAgent2Button.on('click', { agentId: 'agent2' }, agentClick);
+  $connectAgent1Button.on('click', { agentId: 'agent1' }, agentClickHandler);
+  $connectAgent2Button.on('click', { agentId: 'agent2' }, agentClickHandler);
   $hangupCallButton.on('click', hangUp);
   $dialAgent2Button.on('click', dialAgent2);
-
-  function dialAgent2() {
-    $.post('/conference/' + currentAgentId + '/call')
-  }
-
-  function agentClick(e) {
-    var agentId = e.data.agentId;
-    disableConnectButtons(true);
-    fetchToken(agentId);
-  }
-
-  function disableConnectButtons(disable) {
-    $connectAgent1Button.prop('disabled', disable);
-    $connectAgent2Button.prop('disabled', disable);
-  }
 
   function fetchToken(agentId) {
     $.post('/' + agentId + '/token', {}, function(data) {
@@ -38,6 +23,57 @@ $(function() {
 
   function connectClient(token) {
     Twilio.Device.setup(token);
+  }
+
+  Twilio.Device.ready(function (device) {
+    updateCallStatus("Ready");
+    agentConnectedHandler(device._clientName);
+  });
+
+  // Callback for when Twilio Client receives a new incoming call
+  Twilio.Device.incoming(function(connection) {
+    currentConnection = connection;
+    updateCallStatus("Incoming support call");
+
+    // Set a callback to be executed when the connection is accepted
+    connection.accept(function() {
+      updateCallStatus("In call with customer");
+      $answerCallButton.prop('disabled', true);
+      $hangupCallButton.prop('disabled', false);
+      $dialAgent2Button.prop('disabled', false);
+    });
+
+    // Set a callback on the answer button and enable it
+    $answerCallButton.click(function() {
+      connection.accept();
+    });
+    $answerCallButton.prop('disabled', false);
+  });
+
+  /* Report any errors to the call status display */
+  Twilio.Device.error(function (error) {
+    updateCallStatus("ERROR: " + error.message);
+    disableConnectButtons(false);
+  });
+
+  // Callback for when the call finalizes
+  Twilio.Device.disconnect(function(connection) {
+    callEndedHandler(connection.device._clientName);
+  });
+
+  function dialAgent2() {
+    $.post('/conference/' + currentAgentId + '/call')
+  }
+
+  /* End a call */
+  function hangUp() {
+    Twilio.Device.disconnectAll();
+  }
+
+  function agentClickHandler(e) {
+    var agentId = e.data.agentId;
+    disableConnectButtons(true);
+    fetchToken(agentId);
   }
 
   function agentConnectedHandler(agentId) {
@@ -60,46 +96,9 @@ $(function() {
     updateCallStatus("Connected as: " + agentId);
   }
 
-  Twilio.Device.ready(function (device) {
-    updateCallStatus("Ready");
-    agentConnectedHandler(device._clientName);
-  });
-
-  /* Report any errors to the call status display */
-  Twilio.Device.error(function (error) {
-    updateCallStatus("ERROR: " + error.message);
-    disableConnectButtons(false);
-  });
-
-
-  // Callback for when Twilio Client receives a new incoming call
-  Twilio.Device.incoming(function(connection) {
-    currentConnection = connection;
-    updateCallStatus("Incoming support call");
-
-    // Set a callback to be executed when the connection is accepted
-    connection.accept(function() {
-      updateCallStatus("In call with customer");
-      $answerCallButton.prop('disabled', true);
-      $hangupCallButton.prop('disabled', false);
-      $dialAgent2Button.prop('disabled', false);
-    });
-
-    // Set a callback on the answer button and enable it
-    $answerCallButton.click(function() {
-      connection.accept();
-    });
-    $answerCallButton.prop('disabled', false);
-  });
-
-  // Callback for when the call finalizes
-  Twilio.Device.disconnect(function(connection) {
-    callEndedHandler(connection.device._clientName);
-  });
-
-  /* End a call */
-  function hangUp() {
-    Twilio.Device.disconnectAll();
+  function disableConnectButtons(disable) {
+    $connectAgent1Button.prop('disabled', disable);
+    $connectAgent2Button.prop('disabled', disable);
   }
 
   function updateCallStatus(status) {
